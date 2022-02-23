eee19c035a19ffa6d41cf29d2fe74b07f56d2a70
HookedBehemoth <LuisScheurenbrand@gmail.com>
2022-02-23 13:28:22 +0100

    inline all functions

--- src/align.rs
+++ src/align.rs
@@ -1,3 +1,4 @@
+#[inline]
 pub fn align_up(len: usize, align: usize) -> usize {
     len.wrapping_add(align).wrapping_sub(1) & !align.wrapping_sub(1)
 }
--- src/types.rs
+++ src/types.rs
@@ -2,12 +2,14 @@ use crate::align::align_up;
 use byteorder::{BigEndian, ByteOrder};
 
 pub trait OscType {
+    #[inline]
     fn width(&self) -> usize
     where
         Self: Sized,
     {
         std::mem::size_of::<Self>()
     }
+    #[inline]
     fn tag_width(&self) -> usize {
         std::mem::size_of::<u8>()
     }
@@ -16,27 +18,33 @@ pub trait OscType {
 }
 
 impl OscType for i32 {
+    #[inline]
     fn tag(&self) -> u8 {
         b'i'
     }
+    #[inline]
     fn encode(&self, buf: &mut [u8]) {
         BigEndian::write_i32(buf, *self);
     }
 }
 
 impl OscType for f32 {
+    #[inline]
     fn tag(&self) -> u8 {
         b'f'
     }
+    #[inline]
     fn encode(&self, buf: &mut [u8]) {
         BigEndian::write_f32(buf, *self);
     }
 }
 
 impl OscType for bool {
+    #[inline]
     fn width(&self) -> usize {
         0
     }
+    #[inline]
     fn tag(&self) -> u8 {
         if *self {
             b'T'
@@ -44,18 +52,22 @@ impl OscType for bool {
             b'F'
         }
     }
+    #[inline]
     fn encode(&self, _buf: &mut [u8]) {
         // do nothing
     }
 }
 
 impl OscType for &[u8] {
+    #[inline]
     fn width(&self) -> usize {
         align_up(std::mem::size_of::<i32>() + self.len(), 4)
     }
+    #[inline]
     fn tag(&self) -> u8 {
         b'b'
     }
+    #[inline]
     fn encode(&self, buf: &mut [u8]) {
         BigEndian::write_i32(&mut buf[..4], self.len() as i32);
         buf[4..4 + self.len()].copy_from_slice(self);
@@ -63,12 +75,15 @@ impl OscType for &[u8] {
 }
 
 impl OscType for &str {
+    #[inline]
     fn width(&self) -> usize {
         align_up(self.len() + 1, 4)
     }
+    #[inline]
     fn tag(&self) -> u8 {
         b's'
     }
+    #[inline]
     fn encode(&self, buf: &mut [u8]) {
         buf[..self.len()].copy_from_slice(self.as_bytes());
         buf[self.len()] = 0;
